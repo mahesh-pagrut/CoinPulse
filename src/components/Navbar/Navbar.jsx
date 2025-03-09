89ac8106cf11812e5222a097836c52c7f238d342
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-03-09 15:43:13 +0530

    navbar component setup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,27 @@
 import React from 'react'
 import './Navbar.css'
 import logo from "../../assets/logo.png"
+import arrow from "../../assets/arrow_icon.png"
 
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <img src={logo}  alt='logo-on-navbar' />
+        <ul>
+            <li>Home</li>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Blogs</li>
+        </ul>
+        <div className="nav-right">
+            <select>
+                <option value="USD"></option>
+                <option value="EURO"></option>
+                <option value="INR"></option>
+            </select>
+            <button>Sign Up <img src={arrow} alt="arrow-icon"/></button>
+        </div>
     </div>
   )
 }

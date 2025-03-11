0417510debe283a6098d0f478a9ae9c0c0d3adc3
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-03-11 23:54:32 +0530

    added duggention functionality ot search box

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './Home.css'
 import { CoinContext } from '../../context/CoinContext'
+import {Link} from 'react-router-dom'
+
 export const Home = () => {
 
     const {allCoin, currency} = useContext(CoinContext);
@@ -36,7 +38,13 @@ export const Home = () => {
             <p>Welcome to the world's largest cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
 
             <form onSubmit={searchHandler}>
-                <input onChange={inputHandler} value={input} type="text" placeholder='Search crypto...' required/>
+                
+                <input onChange={inputHandler} list='coinlist' value={input} type="text" placeholder='Search crypto...' required/>
+
+                <datalist id="coinlist">
+                    {allCoin.map((item, index) => (<option key={index} value={item.name}/>))}
+                </datalist>
+
                 <button type='submit'>Search</button>
             </form>
         </div>
@@ -50,7 +58,7 @@ export const Home = () => {
             </div>
             {
                 displayCoin.slice(0,15).map((item, index) =>(
-                    <div className='table-layout' key={index}>
+                    <Link to={`/coin/${item.id}`} className='table-layout' key={index}>
                         <p>{item.market_cap_rank}</p>
                         <div>
                             <img src={item.image} alt='coin-image'/>
@@ -60,7 +68,7 @@ export const Home = () => {
                         <p className={item.price_change_percentage_24h > 0?"green" : "red"}>
                             {Math.floor(item.price_change_percentage_24h*100)/100}</p>
                         <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
-                    </div>
+                    </Link>
                 ))
             }
 

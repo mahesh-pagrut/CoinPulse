27a73fca3630d20d015302886c089f55a8244b91
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-03-11 20:54:34 +0530

    footer setup css setup and added meta tags in index html

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div>
+        <p>Copyright @ 2024, CoinPulse - Get the pulse of the crypto market - All rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
